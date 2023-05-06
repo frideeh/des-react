70cfb21a5d5b8fd83553af4780fccd44ea8abb57
frideeh <fardieh.ramezani80@gmail.com>
2023-05-06 18:46:16 +0430

    First Push

---
+++ desmassenger/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+var a = 0
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color: a === 1 ? 'red' : 'black'}}>hello farideh</h1>
+
+    </div>
+  );
+}
+
+export default App;
