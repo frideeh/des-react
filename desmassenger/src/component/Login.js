dec40e48b0869af8c9475ec782486a74a795eed3
MohamadChavoshi <mohamadchavoshi760@gmail.com>
2023-05-07 15:54:45 +0430

    Implement chatRoom base component & Refine code

--- desmassenger/src/component/Login.js
+++ desmassenger/src/component/Login.js
@@ -2,15 +2,10 @@ import '../style/Login.css';
 
 
 
-var a = 0
-
-
 function App() {
   return (
     <div className="App">
-      <h1 style={{color: a === 1 ? 'red' : 'black'}}>hello farideh</h1>
-      <div className="test">test</div>
-
+      <h1>Login page</h1>
     </div>
   );
 }
